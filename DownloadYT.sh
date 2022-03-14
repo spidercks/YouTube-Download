e943ae29b7b707675587772bee9bd5a69af97c67
spidercks <98835437+spidercks@users.noreply.github.com>
2022-03-14 12:49:38 -0300

    Update DownloadYT.sh

--- DownloadYT.sh
+++ DownloadYT.sh
@@ -7,15 +7,15 @@ function youtube()
         pkg install -y curl &> /dev/null
         pkg install -y ffmpeg &> /dev/null
         [[ -d ~/storage ]] || termux-setup-storage
-        curl -L https://yt-dl.org/downloads/latest/youtube-dl -o ${path_bin}youtube-dl &> /dev/null
-        chmod a+rx /data/data/com.termux/files/usr/bin/youtube-dl
+        curl -L https://yt-dl.org/downloads/latest/DownloadYT -o ${path_bin} DownloadYT &> /dev/null
+        chmod a+rx /data/data/com.termux/files/usr/bin/DownloadYT
         > .okay_y
     fi
 
     clear
     printf "\e[1;34m
- 1) MP3/MUSICA
- 2) MP4/VIDEO
+ [1] MP3/MUSICA
+ [2] MP4/VIDEO
 
  ===> \e[1;36m"
     read option
@@ -24,12 +24,12 @@ function youtube()
 
     if [[ ${option} -eq 1 ]]; then
         printf "\e[1;34m Fazendo o download MP3\e[1;36m\n"
-        youtube-dl --extract-audio --audio-format mp3 $url | sed 's/\[/\ [/g;s/\[K/\ [/g;s/Deleting/\ Deleting/g'
+        DownloadYT --extract-audio --audio-format mp3 $url | sed 's/\[/\ [/g;s/\[K/\ [/g;s/Deleting/\ Deleting/g'
         [[ $? -eq 0 ]] && printf "\e[1;34mSucesso!\n"
 
     else
         printf "\e[1;34m Fazendo o download MP4\e[1;36m"
-        youtube-dl ${url}
+        DownloadYT ${url}
         [[ $? -eq 0 ]] && printf "\e[1;34m Sucesso!"
     fi
 }
